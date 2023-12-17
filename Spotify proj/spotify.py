71eb15b30a172158d178880bf012d38ac6a508ce
KwstasKond22 <151690661+KwstasKond22@users.noreply.github.com>
2023-12-17 17:37:41 +0200

    Add files via upload

---
+++ Spotify proj/spotify.py
@@ -0,0 +1,44 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from main import  *
+
+SPOTIPY_CLIENT_ID="6d910e74715d4768a0464011d0d7384c"
+
+SPOTIPY_CLIENT_SECRET="f4069d6ff8144224b002dfd73917dee9"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="ilbk124",
+    )
+)
+user_id = sp.current_user()["id"]
+
+
+
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+
+
+
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
